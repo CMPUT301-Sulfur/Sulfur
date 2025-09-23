1171b82546f1e0293a5bc093b8a97a99c2f85541
Scribbzz <ahaanmandan@gmail.com>
2025-09-23 17:28:30 -0600

    updated project

---
+++ app/src/main/java/com/example/sulfur/Shape.java
@@ -0,0 +1,10 @@
+package com.example.sulfur;
+
+public abstract class Shape {
+    private int x;
+    private int y;
+    public Shape(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
