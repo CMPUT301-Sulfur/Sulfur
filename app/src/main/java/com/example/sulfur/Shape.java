c42f1d63189197669213d9f1d191a635319d6246
jkwatso <156499201+jkwatso@users.noreply.github.com>
2025-09-23 19:03:47 -0600

    Update Shape.java

--- app/src/main/java/com/example/sulfur/Shape.java
+++ app/src/main/java/com/example/sulfur/Shape.java
@@ -3,6 +3,8 @@ package com.example.sulfur;
 public abstract class Shape {
     private int x;
     private int y;
+
+    public String Colour = "Red";
     public Shape(int x, int y){
         this.x = x;
         this.y = y;
